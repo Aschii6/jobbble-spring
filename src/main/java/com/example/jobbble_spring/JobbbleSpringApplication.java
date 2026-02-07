746dd35187b65b4f12ebe44272ae6a054839157b
Aschii6 <danutlechea2003@yahoo.com>
2026-02-07 23:03:56 +0200

    Initial commit

---
+++ src/main/java/com/example/jobbble_spring/JobbbleSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.jobbble_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobbbleSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobbbleSpringApplication.class, args);
+	}
+
+}
