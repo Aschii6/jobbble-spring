33d238daf9b576cb4f12771a856b2b92781ec936
Aschii6 <danutlechea2003@yahoo.com>
2026-02-13 21:16:45 +0200

    Whoops.

--- src/main/java/com/example/jobbble_spring/controllers/ApplicationController.java
+++ src/main/java/com/example/jobbble_spring/controllers/ApplicationController.java
@@ -1,16 +1,16 @@
 package com.example.jobbble_spring.controllers;
 
 import com.example.jobbble_spring.dto.ApplicationResponse;
-import com.example.jobbble_spring.entities.Application;
 import com.example.jobbble_spring.services.ApplicationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@RestController("/applications")
+@RestController
+@RequestMapping("/applications")
 public class ApplicationController {
     @Autowired
     ApplicationService applicationService;
